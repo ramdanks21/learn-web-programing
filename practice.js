7061277e0a101bc1788fb1006d7900f4a80c87f1
ramdan kusumah <ramdankusumah2@gmail.com>
2024-06-13 14:33:36 +0700

    My Journey learning

---
+++ practice.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container ul");
+const li = container.querySelector("li");
+
+const liBaru = document.createElement("li");
+const textL = document.createTextNode("Tester");
+liBaru;
+
+li.appendChild(liBaru);
--- praktek.js
+++ praktek.js
@@ -12,6 +12,7 @@
 
 const container = document.querySelector(".container");
 const jumbo = document.querySelector(".jumbo");
+const thumbs = document.querySelectorAll("thumb");
 container.addEventListener("click", function (e) {
   // cek apakah yang diclik adalah thumb
   if (e.target.className == "thumb") {
@@ -19,5 +20,15 @@ container.addEventListener("click", function (e) {
     jumbo.src = e.target.src;
     // elemen yang besar nya ditambah class fade
     jumbo.classList.add("fade");
+    setTimeout(function () {
+      jumbo.classList.remove("fade");
+    }, 500);
+    thumbs.forEach(function (thumb) {
+      // if (thumb.classList.contains('active')) {
+      //     thumb.classList.remove('.active')
+      //   }
+      thumb.className = "thumb"; //   memanksa semua class thumb
+    });
+    e.target.classList.add("active");
   }
 });
