7709a171ed030e0d8fe048001e2fa48fe0206030
ramdan kusumah <ramdankusumah2@gmail.com>
2024-06-16 08:00:28 +0700

    empty

--- DOM-Manipulasi/dom.js
+++ DOM-Manipulasi/dom.js
@@ -1,22 +1,11 @@
-const liBaru = document.createElement("li");
-const textliBaru = document.createTextNode("Item baru");
-liBaru.appendChild(textliBaru);
-
-// menyimpan elenbaru diposisi sebelum anak ke 2
-const ul = document.querySelector("section#b ul"); //posisi tempat
-const li2 = ul.querySelector("li:nth-child(2)"); //element target li anak ke 2
-
-ul.insertBefore(liBaru, li2);
-
-// removechild
-const sectionA = document.querySelector("section#a");
-const link = document.getElementsByTagName("a")[0];
-sectionA.removeChild(link);
-
-//rep;acechild
-const sectionB = document.querySelector("section#b"); // membatasi secope
-const p4 = sectionB.querySelector("p"); // mengakses tag p disecetion id ='b'
-const h2Baru = document.createElement("h2");
-const textH2Baru = document.createTextNode("Judul Baru");
-h2Baru.appendChild(textH2Baru);
-sectionB.replaceChild(h2Baru, p4);
+const sectionA = document.getElementById("a");
+const p = sectionA.querySelector("p");
+const ig = document.querySelector("img");
+ig.src = "hello brow";
+const news = document.createElement("h2");
+const isi = document.createTextNode("hello");
+news.appendChild(isi);
+p.setAttribute("src", "hello");
+p.src = "bro";
+p.before(news);
+p.parentElement;
